c404784702e538cf7971ec9688a7d6e09817d528
EUNI0101 <acc0830@gmail.com>
2026-03-12 15:50:50 +0900

    Add booking configuration with items and time slots

--- booking-config.js
+++ booking-config.js
@@ -1,1 +1,45 @@
+<script>
+window.TATOA_BOOKING_CONFIG = {
+  cartKey: 'tatoa_booking_cart',
+  bookingPageUrl: 'https://clicktosmile40846.imweb.me/63',
+  cartPageUrl: 'https://clicktosmile40846.imweb.me/62',
+  submitEndpoint: '',
 
+  items: {
+    kissable_lip: {
+      id: 'kissable_lip',
+      title: '키써블립',
+      category: '이벤트',
+      price: 130000,
+      originalPrice: 170000,
+      discountRate: 24,
+      image: 'https://via.placeholder.com/700x700?text=Kissable+Lip',
+      desc1: '화이트데이, 입술에도 작은 설렘을 더해보세요 💋',
+      desc2: '입술 볼륨과 입꼬리 라인을 함께 정돈해 부드럽고 사랑스러운 인상을 완성합니다.'
+    },
+
+    rejubeauck_package: {
+      id: 'rejubeauck_package',
+      title: '리쥬베룩 스타트 체험 패키지',
+      category: '이벤트',
+      price: 390000,
+      originalPrice: 490000,
+      discountRate: 20,
+      image: 'https://via.placeholder.com/700x700?text=Rejubeauck',
+      desc1: '피부 컨디션을 끌어올리는 리쥬베룩 체험 패키지',
+      desc2: '피부결과 탄력 개선에 도움을 주는 프로그램입니다.'
+    }
+  },
+
+  timeSlots: [
+    '11:00','11:30','12:00','12:30','13:00',
+    '13:30','14:00','14:30','15:00','15:30',
+    '16:00','16:30','17:00','17:30','18:00',
+    '18:30','19:00','19:30','20:00','20:30'
+  ],
+
+  closedWeekdays: [0],
+  minDateOffset: 0,
+  maxDateOffset: 60
+};
+</script>
